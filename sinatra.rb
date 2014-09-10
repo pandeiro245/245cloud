11025b9f2d34402d2317583b52690cbc2ebb4f1c
西小倉宏信 <nishiko@mindia.jp>
2014-09-10 04:21:06 +0000

    jsonp

--- sinatra.rb
+++ sinatra.rb
@@ -1,10 +1,5 @@
 require 'sinatra'
-require 'net/http'
-require 'uri'
-require 'dotenv'
 require 'coffee-script'
-
-Dotenv.load
 
 get "/:filename.js" do
   coffee params[:filename].to_sym
@@ -12,11 +7,4 @@ end
 
 get '/' do
   File.open 'public/index.html'
-end
-
-get '/proxy' do
-  raise 'forbidden' unless params[:url].match(/^https:\/\/ruffnote.com\/pandeiro245\/245cloud\/[0-9]*\/download.json$/)
-  headers "Content-Type" => "text/json; charset=utf8"
-  url = params[:url]
-  Net::HTTP.get(URI.parse(url))
 end
