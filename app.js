6ef4ad8f6e6371f49cae4ded615aa59e7c3feed0
Hironobu Nishikokura <nishiko@mindia.jp>
2014-11-08 22:54:39 +0900

    process.env.PORT enabled

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.get("/:filename.js", function (req, res) {
   res.render('views/' + req.params.filename);
 })
 
-var server = app.listen(3000, function () {
+var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address
   var port = server.address().port
   console.log('listening at http://%s:%s', host, port)
