f2af294d5348d9634ef93392ff53a6fe11268f2e
Hironobu Nishikokura <nishiko@mindia.jp>
2015-03-05 22:49:57 +0900

    add config/initializers/omniauth.rb

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
   resources :nicoinfo, only: [:show], constraints: {id: /sm[0-9]+/}
   resources :users, :workloads, :musics
 
+  # omniauth
   match '/auth/:provider/callback' => 'sessions#callback', via: [:get, :post]
   match '/auth/failure' => 'sessions#failure', via: [:get, :post]
+  root 'welcome#index'
 end
