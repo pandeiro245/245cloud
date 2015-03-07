f982bd2ed77dfb1c05ce53931dec7cf652e8c16f
Hironobu Nishikokura <nishiko@mindia.jp>
2015-03-07 15:41:02 +0900

    udpate config/routes

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,11 @@ Rails.application.routes.draw do
 
   match '/auth/:provider/callback' => 'sessions#callback', via: [:get, :post]
   match '/auth/failure' => 'sessions#failure', via: [:get, :post]
+
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+
   root 'welcome#index'
-  resources :users, :workloads, :musics
 end
 
