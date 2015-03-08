abb22c772a1094e30648ff6d7dd79f40088371c7
Hironobu Nishikokura <nishiko@mindia.jp>
2015-03-08 17:11:33 +0900

    WorkloadとMusicのmigrationを追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150307070720) do
+ActiveRecord::Schema.define(version: 20150308080930) do
 
   create_table "auths", force: :cascade do |t|
     t.integer  "user_id",     limit: 4
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20150307070720) do
     t.text     "credentials", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+  end
+
+  create_table "musics", force: :cascade do |t|
+    t.string   "title",       limit: 255
+    t.boolean  "is_fixed",    limit: 1
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "total_count", limit: 4
+    t.text     "key",         limit: 65535
+    t.text     "user_counts", limit: 65535
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,5 +55,15 @@ ActiveRecord::Schema.define(version: 20150307070720) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "workloads", force: :cascade do |t|
+    t.string   "title",      limit: 255
+    t.boolean  "is_done",    limit: 1
+    t.string   "user_hash",  limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.text     "key",        limit: 65535
+    t.integer  "music_id",   limit: 4
+  end
 
 end
