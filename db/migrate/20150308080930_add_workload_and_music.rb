04b1408b33fddd222b35e1ac551ab50ac6b03f35
Hironobu Nishikokura <nishiko@mindia.jp>
2015-03-08 17:11:33 +0900

    WorkloadとMusicのmigrationを追加

---
+++ db/migrate/20150308080930_add_workload_and_music.rb
@@ -0,0 +1,24 @@
+class AddWorkloadAndMusic < ActiveRecord::Migration
+  def change
+    create_table "musics" do |t|
+      t.string   "title"
+      t.boolean  "is_fixed"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.integer  "total_count"
+      t.text     "key"
+      t.text   "user_counts"
+    end
+
+    create_table "workloads" do |t|
+      t.string   "title"
+      t.boolean  "is_done"
+      t.string   "user_hash"
+      t.datetime "created_at"
+      t.datetime "updated_at"
+      t.text     "key"
+      t.integer  "music_id"
+    end
+  end
+end
+
