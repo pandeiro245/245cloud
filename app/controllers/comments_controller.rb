8e0f5301c9237e5788bdbb621d0a5eece255032a
Hironobu Nishikokura <nishiko@mindia.jp>
2015-03-17 21:42:37 +0900

    RoomのSeed追加したりもろもろ

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,24 @@ class CommentsController < ApplicationController
   # GET /rooms/#{room_id}/comments
   def index
     # TODO このAPIのデータを返したら未読数をアップデートする
-    render json: Comment.all
+    room_id = params[:room_id].to_i
+    room_id = nil if room_id == 0
+    @comments = Comment.where(room_id: room_id)
+    render json: @comments
   end
+
+  def create
+    @comment = Comment.new(comment_params)
+    if @comment.save
+      render json: 'ok'
+    else
+      render json: 'ng...'
+    end
+  end
+
+  private
+    def comment_params
+      params.permit(:content)
+    end
 end
+
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+  def icon_url
+    'https://fbcdn-profile-a.akamaihd.net/hprofile-ak-xpa1/v/t1.0-1/c32.32.401.401/s50x50/148782_450079083380_6432972_n.jpg?oh=bdbe1653f8db778471c43c872fcc97d5&oe=55514D90&__gda__=1430901205_655202eb092cf8b4ffff266a03b5e658'
+  end
 end
