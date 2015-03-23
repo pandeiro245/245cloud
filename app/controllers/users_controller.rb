1630eb7e5d7964eb4a1120c342fe0b897363abc6
Hironobu Nishikokura <nishiko@mindia.jp>
2015-03-24 02:07:50 +0900

    add Parsecom

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  def index
-    render json: User.all
+  def show
+    @user = User.find(params[:id]) 
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,17 @@
 class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+
+  has_many :workloads
+
+  def recent_musics
+    Workload.limit(100).order(
+      'id desc'
+    ).where(
+      user_id: self.id
+    ).where(
+      'music_id is not null'
+    ).map{|w| w.music}.uniq!
+  end
 end
 
