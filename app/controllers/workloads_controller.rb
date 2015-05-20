6cb1490530265ca620ce01e833a4aa8de9618d64
Hironobu Nishikokura <nishiko@mindia.jp>
2015-05-21 08:13:41 +0900

    WorkloadControllerの処理をWorkloadモデルに移行

--- app/controllers/workloads_controller.rb
+++ app/controllers/workloads_controller.rb
@@ -1,47 +1,21 @@
 class WorkloadsController < ApplicationController
   def doings
-    @workloads = Workload.where(
-      "created_at > '#{(Time.now - 24.minutes).to_s}'"
-    ).order('id desc').limit(96).map do |workload| 
-      w = JSON.parse(workload.to_json)
-      w['icon_url'] = workload.icon_url
-      w
-    end
-    render json: @workloads.to_json
+    render json: Workload.doings
   end
 
   def chattings
   end
 
   def dones
-    @workloads = Workload.where(
-      "created_at < '#{(Time.now - 24.minutes).to_s}'" # 不要？
-    ).where(
-      is_done: true 
-    ).order('id desc').limit(48).map do |workload| 
-      w = JSON.parse(workload.to_json)
-      w['icon_url'] = workload.icon
-      if workload.music
-        w['artwork_url'] = workload.music.icon
-        w['title']       = workload.music.title
-        w['key']       = workload.key
-      end
-      w
-    end
-    render json: @workloads.to_json
+    render json: Workload.dones
   end
 
   def create
-    workload = Workload.create! # user_id is nilを許容
-    render json: workload
+    render json: Workload.create! # user_id is nilを許容
   end
 
   def complete
-    @workload = Workload.find(params[:id])
-    @workload.is_done = true
-    @workload.number = Workload.where(is_done: true).count + 1 # FIXME
-    @workload.save!
-    render json: "id: #{@workload.id} is completed."
+    render json: Workload.find(params[:id]).complete!
   end
 end
 
--- app/models/workload.rb
+++ app/models/workload.rb
@@ -7,6 +7,40 @@ class Workload < ActiveRecord::Base
     user.present? ? user.icon : "https://ruffnote.com/attachments/24311"
   end
 
+  def complete!
+    @workload.is_done = true
+    @workload.number = Workload.where(is_done: true).count + 1 # FIXME
+    @workload.save!
+    @workload
+  end
+
+  def self.doings
+    Workload.where(
+      "created_at > '#{(Time.now - 24.minutes).to_s}'"
+    ).order('id desc').limit(96).map do |workload| 
+      w = JSON.parse(workload.to_json)
+      w['icon_url'] = workload.icon_url
+      w
+    end
+  end
+
+  def self.dones
+    Workload.where(
+      "created_at < '#{(Time.now - 24.minutes).to_s}'" # 不要？
+    ).where(
+      is_done: true 
+    ).order('id desc').limit(48).map do |workload| 
+      w = JSON.parse(workload.to_json)
+      w['icon_url'] = workload.icon
+      if workload.music
+        w['artwork_url'] = workload.music.icon
+        w['title']       = workload.music.title
+        w['key']       = workload.key
+      end
+      w
+    end
+  end
+
   def self.refresh_numbers
     numbers = {}
     Workload.where(number: nil).dones.order('id asc').each do |workload|
