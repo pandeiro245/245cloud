af34c80b81c503fc991b39fd3c27644a521badd0
Hironobu Nishikokura <nishiko@mindia.jp>
2015-07-22 16:27:21 +0900

    update deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,5 @@
 # config valid only for current version of Capistrano
-lock '3.4.0'
-
-set :application, '245cloud'
-set :repo_url, 'git@github.com:pandeiro245/245cloud.git'
+#lock '3.4.0'
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
