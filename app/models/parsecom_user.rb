c1a16daa5cb4b5e48dbf08e6e98942aa6ae821c7
Hironobu Nishikokura <nishiko@mindia.jp>
2015-08-15 23:20:50 +0900

    ParsecomWorkload.sync and ParsecomUser.sync

--- app/models/parsecom_user.rb
+++ app/models/parsecom_user.rb
@@ -1,4 +1,35 @@
 class ParsecomUser < ParseUser
-  fields :name, :facebook_id_str
+  fields :name, :facebook_id_str, :user_id
+
+  def self.sync refresh = false
+    if refresh
+      parse_users = self.order('createdAt asc')
+    else
+      parse_users = self.where(user_id: nil).order('createdAt asc')
+    end
+    parse_users.each do |parse_user|
+      u = parse_user.attributes
+
+      facebook_id = u['authData']['facebook']['id']
+      email = "#{facebook_id}@245cloud.com"
+      user = User.find_or_create_by(
+        email: email 
+      )
+      user.name = u['name']
+      user.parsecomhash = u['objectId']
+      user.save!
+
+      parse_user.user_id = user.id
+      parse_user.save
+
+      auth = Auth.find_or_initialize_by(
+        provider: 'facebook',
+        uid: facebook_id
+      )
+      auth.user_id = user.id
+      auth.save!
+    end
+    return 'done'
+  end
 end
 
--- app/models/parsecom_workload.rb
+++ app/models/parsecom_workload.rb
@@ -1,3 +1,58 @@
 class ParsecomWorkload < ParseResource::Base
   fields :workload_id, :sc_id, :mc_id, :yt_id, :et_id, :sm_id, :is_done, :number, :artwork_url, :title
+
+  def self.sync
+    self.where(workload_id: nil).order('createdAt asc').each do |parse_workload|
+      workload = parse_workload.attributes
+
+      id = nil
+      key = nil
+      if id = workload['sc_id']
+        id = id.to_i.to_s
+        key = 'sc'
+      elsif id = workload['mc_id']
+        key = 'mc'
+      elsif id = workload['yt_id']
+        key = 'yt'
+      elsif id = workload['et_id']
+        key = 'et'
+      elsif id = workload['sm_id']
+        key = 'sm'
+      end
+
+      if id
+        music = Music.find_or_create_by(
+          key: "#{key}:#{id}"
+        )
+        music.title = workload['title']
+        music.icon = workload['artwork_url']
+        music.save!
+      end
+     
+      next unless workload['user']
+
+      workload2 = Workload.find_or_initialize_by(
+        parsehash: workload['objectId']
+      )
+
+
+      user = User.find_by(parsecomhash: workload['user']['objectId'])
+
+      unless workload2.id
+        workload2.user_id = user.id
+        workload2.created_at =  workload['createdAt'].to_time
+        workload2.number = workload['number']
+        workload2.place_id = workload['place_id']
+        workload2.status = workload['is_done'] || 0
+        workload2.music_id = music.id if music
+        workload2.save!
+      end
+      parse_workload.workload_id = workload2.id
+      parse_workload.save
+
+      puts "done: workload.id = #{workload2.id}"
+    end
+    puts 'done'
+  end
 end
+
