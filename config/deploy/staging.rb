b723214feb9dcf453571570d9a96cc56bcf65eea
Hironobu Nishikokura <nishiko@mindia.jp>
2015-08-19 14:38:36 +0900

    typo

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -64,4 +64,4 @@ set :deploy_to, '/var/www/245cloud'
 set :application, '245cloud'
 set :unicorn_config_path, "#{current_path}/config/unicorn.rb"
 set :repo_url, 'git@github.com:pandeiro245/245cloud.git'
-set :branch, 'mongodb'
+set :branch, 'mongo'
