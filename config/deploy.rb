985fbe71dc53b160a54cf3fef2e3a6127dc77470
Hironobu Nishikokura <nishiko@mindia.jp>
2015-08-21 23:52:24 +0900

    http://lv4.hateblo.jp/entry/2014/07/09/180400

--- config/deploy.rb
+++ config/deploy.rb
@@ -48,6 +48,9 @@ end
 
 set :rbenv_type, :user
 set :rbenv_ruby, '2.2.0'
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails}
+set :rbenv_roles, :all
 
 after 'deploy:publishing', 'deploy:restart'
 namespace :deploy do
