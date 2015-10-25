2ae3eb5823548cd88fe7b22cd76b40f224bd6820
Hironobu Nishikokura <nishiko@mindia.jp>
2015-10-25 14:34:41 +0900

    rm UsersController

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  get '/musics/:id' => 'musics#show'
   root 'welcome#index'
   resources :nicoinfo, only: [:show], constraints: {id: /sm[0-9]+/}
-
-  #comfy_route :cms_admin, :path => '/admin'
-  #comfy_route :cms, :path => '/', :sitemap => false
-
-  get '/:id' => 'users#show'
 end
