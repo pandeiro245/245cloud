324d56b6382a7e7f00374560f5bab2bc71e80f16
Hironobu Nishikokura <nishiko@mindia.jp>
2015-11-22 23:38:53 +0900

    add Parsecom::Workload

--- app/models/parsecom/workload.rb
+++ app/models/parsecom/workload.rb
@@ -1,9 +1,11 @@
-class Parsecom::Comment < ParseResource::Base
+class Parsecom::Workload < ParseResource::Base
   fields :user, :icon_url
 
   def self.sync_icon_url
-    self.where(icon_url: nil).each do |c|
-      c.sync_icon_url
+    [nil, 'https://graph.facebook.com/undefined/picture?height=40&width=40'].each do |icon_url|
+      self.where(icon_url: icon_url).each do |c|
+        c.sync_icon_url
+      end
     end
     self.sync_icon_url
   end
