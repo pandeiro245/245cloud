3090f5cccff01bde0c43ac6cdb5d3467e74eecca
Hironobu Nishikokura <nishiko@mindia.jp>
2016-02-21 02:00:14 +0900

    fix api/access_logs#create

--- app/controllers/api/access_logs_controller.rb
+++ app/controllers/api/access_logs_controller.rb
@@ -1,8 +1,10 @@
 class Api::AccessLogsController < ApplicationController
-  facebook_id = current_user ? current_user.facebook_id : nil
-  access_log = AccessLog.create!(
-    facebook_id: facebook_id,
-    url: params[:url]
-  ).decorate
-  render json: access_log
+  def create
+    facebook_id = current_user ? current_user.facebook_id : nil
+    access_log = AccessLog.create!(
+      facebook_id: facebook_id,
+      url: params[:url]
+    )
+    render json: access_log
+  end
 end
