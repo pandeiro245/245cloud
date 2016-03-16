763a1aabfe36e05b4bf3619198cc500307c60a25
Hironobu Nishikokura <nishiko@mindia.jp>
2016-03-16 21:57:11 +0900

    rebase ミスを修正

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-<<<<<<< HEAD
     is_redirect = false
     [:alert, :timecrowd].each do |sym|
       if params[sym]
@@ -13,8 +12,6 @@ class WelcomeController < ApplicationController
       is_redirect = true
     end
     redirect_to :root if is_redirect
-=======
->>>>>>> parse.com系のファイルをがっつり削除
   end
 end
 
