7dd76b946fb90ba27377de9c57ba08c8b5fee973
J.Fukaya <fukajun.shark@gmail.com>
2016-07-09 19:29:51 +0900

    Success facebookloing (mock) on feature spec

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -1,37 +1,32 @@
 require 'rails_helper'
 
-def browser_log
-  puts '--- error log ---'
-   p page.driver.error_messages
-  puts '--- console log ---'
-   p page.driver.console_messages
-end
-
-# FIXME: 実際にログイン処理を実装できてるわけでない
 def login(user)
   OmniAuth.config.test_mode = true
   OmniAuth.config.mock_auth[:facebook] = OmniAuth::AuthHash.new({
     :provider => 'facebook',
     :uid => '123545'
   })
-  #post '/auth/faceboo/callback', screen_name: user.facebook_id
+  find("input[value='facebookログイン']").click
 end
 
 feature 'TOPページ' do
-  scenario 'トップページが表示する' do
-    visit '/'
-
-    browser_log
-    expect(page).to have_content '245cloudは24分間、自分の作業に集中'
-    # Facebookログインボタン
-    expect(page).to have_css("input[value='facebookログイン']")
-  end
-
   context 'ログイン前' do
     let(:user) { FactoryGirl.create(:user) }
+
+    scenario 'トップページが表示する' do
+      visit '/'
+      expect(page).to have_content '245cloudは24分間、自分の作業に集中'
+      # Facebookログインボタン
+      expect(page).to have_css("input[value='facebookログイン']")
+      page.save_screenshot 'screenshot0.png'
+    end
+
     scenario 'Facebookでログインする' do
       visit '/'
       login(user)
+      expect(page).to have_content 'おまかせ'
+      expect(page).to have_content '無音'
+      page.save_screenshot 'screenshot1.png'
     end
   end
 end
