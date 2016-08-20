bcad62b69bbdd3cbb33fcd8d21c80db2c73f1d82
Hironobu Nishikokura <nishiko@mindia.jp>
2016-08-20 14:11:32 +0900

    mv react-native NishikoCloud

--- NishikoCloud/index.ios.js
+++ NishikoCloud/index.ios.js
@@ -17,7 +17,7 @@ class NishikoCloud extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Welcome to 245cloud!
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
