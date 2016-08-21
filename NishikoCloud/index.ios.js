1fbd53a6287b0b7d1fe67463c91ccf54a664547f
Hironobu Nishikokura <nishiko@mindia.jp>
2016-08-21 17:21:18 +0900

    add https://github.com/inProgress-team/react-native-youtube

--- NishikoCloud/index.ios.js
+++ NishikoCloud/index.ios.js
@@ -95,6 +95,24 @@ class NishikoCloud extends Component {
           <Image source={nomusic} style={{width: 150, height: 30}} />
           </TouchableHighlight>
         </Text>
+        {'\n'}{'\n'}
+        <YouTube
+          ref="youtubePlayer"
+          videoId="KVZ-P-ZI6W4" // The YouTube video ID
+          play={true}           // control playback of video with true/false
+          hidden={false}        // control visiblity of the entire view
+          playsInline={true}    // control whether the video should play inline
+          loop={false}          // control whether the video should loop when ended
+
+          onReady={(e)=>{this.setState({isReady: true})}}
+          onChangeState={(e)=>{this.setState({status: e.state})}}
+          onChangeQuality={(e)=>{this.setState({quality: e.quality})}}
+          onError={(e)=>{this.setState({error: e.error})}}
+          onProgress={(e)=>{this.setState({currentTime: e.currentTime, duration: e.duration})}}
+
+          style={{alignSelf: 'stretch', height: 300, backgroundColor: 'black', marginVertical: 10}}
+        />
+
         </Text>
       </ScrollView>
     );
