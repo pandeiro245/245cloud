c69850c0cb98ac749e4c36131fedf19df5684486
EC2 Default User <nishiko@mindia.jp>
2019-07-08 12:37:16 +0000

    Timezone is Tokyo

--- config/application.rb
+++ config/application.rb
@@ -14,5 +14,7 @@ module NishikoCloud
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    Time.zone = 'Tokyo'
   end
 end
