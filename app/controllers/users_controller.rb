51e49e62bf560cfccbb1344f11ca410a9dcb61cc
Nishikokura Hironobu <nishiko@mindia.jp>
2021-08-07 05:54:15 +0000

    save image when twitter_login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -51,6 +51,7 @@ class UsersController < ApplicationController
     end
     user.twitter_id ||= twitter_id
     user.save!
+    `wget -O public/images/profile/#{user.id}.jpg #{auth_hash[:info][:image]}`
     sign_in(user)
   end
 
