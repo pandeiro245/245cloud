f332d01b572a1f9da3e8982f406c94d5ac5f481a
Nishikokura Hironobu <nishiko@mindia.jp>
2021-08-09 04:38:16 +0000

    update workload if chatting

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
 class HomeController < ApplicationController
   def index
-    # sign_out(current_user)
+    if current_user.present? && current_user.status == 'chatting'
+      current_user.chatting.to_done!
+    end
   end
 
   def redirect
