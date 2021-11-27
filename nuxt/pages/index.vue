c587da7b368e90a4203c5b199249b43c3d9debdd
Nishikokura Hironobu <nishiko@mindia.jp>
2021-11-27 04:34:24 +0000

    nishikocloud-nuxt => nuxt

---
+++ nuxt/pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<!--
+	<div id="app">
+		{{ message }}
+	</div>
+	<h1 id ="app">{{ message }}</h1>
+	-->
+	<h1 id ="app">hoge3</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({})
+
+// var app = new Vue({
+//   el: '#app',
+//   data: {
+//     message: 'Hello Vue!'
+//   }
+// })
+</script>
