ca75f8b8812016ed9e27abdfafb6cde17949d322
nishiko <nishiko@mindia.jp>
2023-05-06 13:04:25 +0900

    bundle exec brakeman -q -w2

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -6,7 +6,18 @@ class HomeController < ApplicationController
   end
 
   def redirect
-    redirect_to params[:url]
+    if is_valid_url?(params[:url])
+      redirect_to params[:url]
+    else
+      redirect_to root_path
+    end
+  end 
+
+  private
+
+  def is_valid_url?(url)
+    # 正規表現を使用して、httpまたはhttpsプロトコルで始まるかどうかを確認します
+    !!(url =~ /\A(http|https):\/\//)
   end
 end
 
