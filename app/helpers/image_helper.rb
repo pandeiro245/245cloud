425de7ee8c6929b8d937ec8639116723f6f4d3c2
nishiko <nishiko@mindia.jp>
2023-05-06 12:52:16 +0900

    for lint

--- app/helpers/image_helper.rb
+++ app/helpers/image_helper.rb
@@ -1,5 +1,8 @@
+# frozen_string_literal: true
+
+# Module for working with images.
 module ImageHelper
   def button_muon
-    image_tag "https://ruffnote.com/attachments/24921"
+    image_tag 'https://ruffnote.com/attachments/24921'
   end
 end
