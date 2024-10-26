abbdcdd9f0530d4a7ac119bc74a4b520064e6bec
nishiko <nishiko@mindia.jp>
2024-10-26 23:10:21 +0900

    dont use wget

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -49,7 +49,7 @@ class UsersController < ApplicationController
     end
     user.twitter_id ||= twitter_id
     user.save!
-    `wget -O public/images/profile/#{user.id}.jpg #{auth_hash[:info][:image]}`
+    user.save_image_from_twitter(auth_hash)
     sign_in(user)
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -42,6 +42,32 @@ class User < ActiveRecord::Base
     w
   end
 
+  def save_image_from_twitter(auth_hash)
+    image_url = auth_hash[:info][:image]
+    uri = URI.parse(image_url)
+
+    unless uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+      raise "Invalid URL: #{image_url}"
+    end
+
+    file_path = "public/images/profile/#{id}.jpg"
+
+    Net::HTTP.start(uri.host, uri.port, use_ssl: uri.scheme == 'https') do |http|
+      request = Net::HTTP::Get.new(uri)
+
+      http.request(request) do |response|
+        case response
+        when Net::HTTPSuccess
+          File.open(file_path, 'wb') do |file|
+            response.read_body { |chunk| file.write(chunk) }
+          end
+        else
+          raise "Failed to download image: #{response.message}"
+        end
+      end
+    end
+  end
+
   def email_required?
     false
   end
