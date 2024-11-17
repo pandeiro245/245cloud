52ee222f5006cab5759688c527ca0c82b5b57750
nishiko <nishiko@mindia.jp>
2024-11-17 12:24:46 +0000

    rubocop -a

--- app/models/workload.rb
+++ app/models/workload.rb
@@ -186,7 +186,7 @@ class Workload < ActiveRecord::Base
   end
 
   def disp
-    return "#{hm}" if  created_at  + POMOTIME + CHATTIME > Time.zone.now
+    return "#{hm}" if created_at + POMOTIME + CHATTIME > Time.zone.now
     "#{hm} #{number}回目(週#{weekly_number}回)"
   end
 
@@ -196,7 +196,7 @@ class Workload < ActiveRecord::Base
 
   def within_time_limit?
     time_diff = (created_at + POMOTIME + CHATTIME) - Time.zone.now
-    time_diff.to_i > -((POMOTIME + CHATTIME).to_i)
+    time_diff.to_i > -(POMOTIME + CHATTIME).to_i
   end
 
   def self.for1027
